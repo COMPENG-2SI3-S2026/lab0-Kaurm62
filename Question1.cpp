8197e221b053276506c4cc09fb921bbaae3df325
Manpreet <kaurm62@mcmaster.ca>
2026-07-30 02:11:54 -0400

    Completed Lab 0

--- Question1.cpp
+++ Question1.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int add(int a, int b)
 {
-	// complete the integer addition here and return the integer value
-	
+    // complete the integer addition here and return the integer value
+    return a + b;
 }
